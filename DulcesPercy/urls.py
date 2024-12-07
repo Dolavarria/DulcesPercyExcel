01da4245d6c0ee7796c8080b4a31bbe4ea75e8ff
Diego Olavarria <dolavarria@utem.cl>
2024-12-07 20:05:19 -0300

    Actualización de rutas y mejoras en la plantilla de inicio, incluyendo estilos y estructura

--- DulcesPercy/urls.py
+++ DulcesPercy/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('descargar/<str:tipo>/', views.descargar_libro, name='descargar_libro'),
     path('librodiario/', views.libro_diario, name='libro_diario'),
     path('', views.home, name='home'),
-    path('verregistro/', views.ver_registro, name='ver_registro'),
+    path('registro/', views.ver_registro, name='ver_registro'),
 
 ]
